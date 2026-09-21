2ad0543bce8845f0166b771db716ac5cad257490
ShriRaghavi27 <rajan.shriraghavi@gmail.com>
2026-09-21 11:01:05 +0530

    First Push

---
+++ com.seleniumProject/src/test/java/pac2/CreateorgTest.java
@@ -0,0 +1,17 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class CreateorgTest {
+	
+	@Test
+	public void createorgTest() {
+		System.out.println("Excute createorgTest ");
+	}
+	@Test
+	public void modifyorgTest() {
+		System.out.println("Excute modifyorgTest ");
+	}
+
+
+}
